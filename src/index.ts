b4fc3892a37bc7cdfeecc8a0e602a6929dfef38c
Pavlo Panukhnyk <panukhnykpa@gmail.com>
2025-06-30 13:04:37 +0200

    add test 2, 3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Hello, world')
+const division =(a: number, b: number): number => {
+    return a / b;
+}
+
+console.log(division(6, 0))
+
+//test 3
+
+interface Car{
+    make: string;
+    model: string;
+    year: number;
+    isElectric: boolean;
+}
+const displayCarInfo = (car: Car): void => {
+console.log('Car Information:');
+console.log(`Make: ${car.make}`);   
+console.log(`Model: ${car.model}`);
+console.log(`Year: ${car.year}`);
+console.log(`Is Electric: ${car.isElectric ? 'Yes' : 'No'}`);
+}
+
+const myCar: Car = {
+    make: 'Tesla',
+    model: 'Model S',
+    year: 2022,
+    isElectric: true
+};
+displayCarInfo(myCar);
